a1d128e463d0ccb5b0409652c99a1050cceab5f3
Matheus Rodrigues <matheushehe007@gmail.com>
2025-08-22 19:17:18 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,16 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+//liberado apenas a testes
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
